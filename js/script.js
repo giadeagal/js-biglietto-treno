3bec4201e9a733e7fb3d4f922e10d2d1b88b51d9
Gianluca <gl.galassini@gmail.com>
2021-05-27 17:19:13 +0200

    sconto minori

--- js/script.js
+++ js/script.js
@@ -5,5 +5,10 @@
 
 var age = parseInt(prompt("Inserisci la tua età"));
 var journey = parseInt(prompt("Inserisci la lunghezza del tuo tragitto in chilometri"));
-var price = (journey * 0.21);
+var price = journey * 0.21;
+
 document.getElementById("printer").innerHTML= "Il prezzo del biglietto selezionato è " + price.toFixed(2) + " €";
+
+if (age > 18) {
+    document.getElementById("printer").innerHTML= "Il prezzo del biglietto selezionato è " + (price * 80 / 100).toFixed(2) + " €"
+}
